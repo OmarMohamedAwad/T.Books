2dfa810ef6f3bb9803635339434706e6bb238244
Omar.M.Awad <omar.awad825@gmail.com>
2021-03-21 11:41:46 +0200

    Edit variable name

--- Backend/modules/author/routes/AuthorRoute.js
+++ Backend/modules/author/routes/AuthorRoute.js
@@ -1,26 +1,26 @@
 const express = require('express')
-const AutherAccessController = require('../controllers/AuthorController')
+const AutherController = require('../controllers/AuthorController')
 
 const autherRouter = express.Router()
 
 autherRouter.get("/", (request, response, next)=> {
-    AutherAccessController.index(request, response, next);   
+    AutherController.index(request, response, next);   
 }) 
 
 autherRouter.post("/", async (request, response, next)=> {
-    AutherAccessController.store(request, response, next);
+    AutherController.store(request, response, next);
 })
 
 autherRouter.get("/:id",async (request, response, next)=> {
-    AutherAccessController.show(request, response, next);
+    AutherController.show(request, response, next);
 })
 
 autherRouter.delete("/:id", async (request, response, next)=> {
-    AutherAccessController.destroy(request, response, next);
+    AutherController.destroy(request, response, next);
 })
 
 autherRouter.patch("/:id", async (request, response, next)=> {
-    AutherAccessController.update(request, response, next);
+    AutherController.update(request, response, next);
 })
 
 module.exports = autherRouter;
