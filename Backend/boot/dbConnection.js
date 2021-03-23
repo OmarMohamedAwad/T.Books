d67b3c33ec3296f86b59603dd6122d1462f556bf
Aalaa Habib <aalaahabib.ah@gmail.com>
2021-03-23 12:42:33 +0200

    first commit

--- Backend/boot/dbConnection.js
+++ Backend/boot/dbConnection.js
@@ -1,1 +1,14 @@
+require("../boot/requires");
+const mongoose = require('mongoose')
 
+const MONGODB_URL = process.env.MONGODB_URL
+
+mongoose.connect(MONGODB_URL,{ 
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+    },(error)=>{
+        if (error) console.log(error);
+        else console.log("Connected to DB");  
+})
