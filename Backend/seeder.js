4874c0f94778a1dd4e7dc25683f79c5b50d50640
Aalaa Habib <aalaahabib.ah@gmail.com>
2021-03-24 19:29:27 +0200

    Make Seeder to insert/delete date into/from DB

---
+++ Backend/seeder.js
@@ -0,0 +1,62 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const colors = require('colors');
+
+'use strict';
+//load env 
+const PORT=3000
+const MONGODB_URL="mongodb://localhost:27017/trending_books"
+
+//load user model 
+const User = require('./modules/user/models/User');
+/***You Can add Your Model Link here please ***/
+
+//conect to DataBase 
+mongoose.connect(MONGODB_URL,{ 
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+    },(error)=>{
+        if (error) console.log(error);
+        else console.log("Connected to DB");  
+})
+
+//read json files which have data to insert into DataBase
+const users = JSON.parse(  fs.readFileSync(`${__dirname}/_data/user.json`, 'utf-8'));
+/***You Can add Your json File (put data as a json file in folder _data) Link here please  ***/
+
+
+//import into DB
+const importData = async()=>{
+    try{
+        await User.create(users);
+        /*** Create Your Model.create(fileJsonVariabe) ***/
+        console.log('Data Imported Into DB....'.green.inverse);
+        process.exit();
+    }
+    catch (err){
+        console.error(err);
+    }
+}
+//Delete Data 
+const deleteData = async()=>{
+    try{
+        await User.deleteMany();
+          /*** Create Your Model.deleteMany(fileJsonVariabe) ***/
+        console.log('Data Destoryed...'.red.inverse);
+        process.exit();
+    }
+    catch (err){
+        console.error(err);
+    }
+}
+
+//To Run The File 
+const command = process.argv[2];
+if(command === '-i' || command === 'import'){
+    importData();
+}
+else if(command === '-d' || command === 'delete'){
+    deleteData();
+}
