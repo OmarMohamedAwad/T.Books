832a136346b393ebf57b800e9cab904cafc506f3
Omar.M.Awad <omar.awad825@gmail.com>
2021-04-01 02:52:05 +0200

    Create dash auth structure and build nav and sidebar

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { IndexComponent } from './modules/author/admin/index/index.component';
+import { SidebarComponent } from './modules/author/admin/sidebar/sidebar.component';
+import { StoreComponent } from './modules/author/admin/store/store.component';
+import { DeleteComponent } from './modules/author/admin/delete/delete.component';
+import { UpdateComponent } from './modules/author/admin/update/update.component';
+import { NavbarAdminComponent } from './modules/author/admin/navbar-admin/navbar-admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    SidebarComponent,
+    StoreComponent,
+    DeleteComponent,
+    UpdateComponent,
+    NavbarAdminComponent
   ],
   imports: [
     BrowserModule
---
+++ front-end/src/app/modules/author/admin/delete/delete.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-delete',
+  templateUrl: './delete.component.html',
+  styleUrls: ['./delete.component.css']
+})
+export class DeleteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
