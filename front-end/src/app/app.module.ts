b089c0bcd10e96c47236879cb719721b8b40859d
Omar.M.Awad <omar.awad825@gmail.com>
2021-04-01 22:57:38 +0200

    Add auther and edit and delete

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { IndexComponent } from './modules/author/admin/index/index.component';
-import { SidebarComponent } from './modules/author/admin/sidebar/sidebar.component';
-import { StoreComponent } from './modules/author/admin/store/store.component';
-import { DeleteComponent } from './modules/author/admin/delete/delete.component';
-import { UpdateComponent } from './modules/author/admin/update/update.component';
-import { NavbarAdminComponent } from './modules/author/admin/navbar-admin/navbar-admin.component';
+import { IndexComponent } from './modules/admin layout/author/index/index.component';
+import { SidebarComponent } from './modules/admin layout/shared/sidebar/sidebar.component';
+import { StoreComponent } from './modules/admin layout/author/store/store.component';
+import { DeleteComponent } from './modules/admin layout/author/delete/delete.component';
+import { UpdateComponent } from './modules/admin layout/author/update/update.component';
+import { NavbarAdminComponent } from './modules/admin layout/shared/navbar-admin/navbar-admin.component';
 
 @NgModule({
   declarations: [
--- front-end/src/app/modules/admin layout/author/index/index.component.ts
+++ front-end/src/app/modules/admin layout/author/index/index.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IndexComponent implements OnInit {
 
+
   constructor() { }
 
   ngOnInit(): void {
