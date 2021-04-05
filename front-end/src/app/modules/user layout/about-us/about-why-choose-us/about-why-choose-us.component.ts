b915238337ba33ac6ae6edc261e1f803b4e0f3ea
Aalaa Habib <aalaahabib.ah@gmail.com>
2021-04-06 00:12:38 +0200

    refactor angular code

--- front-end/src/app/modules/user layout/about-us/about-why-choose-us/about-why-choose-us.component.ts
+++ front-end/src/app/modules/user layout/about-us/about-why-choose-us/about-why-choose-us.component.ts
@@ -6,7 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about-why-choose-us.component.css']
 })
 export class AboutWhyChooseUsComponent implements OnInit {
-
+  cardData:any=[
+    {icon:"far fa-hand-spock icon",title:"Trusted By Thousands",desc:"Aliquam dictum elit vitae mauris facilisis at dictum urna dignissim donec vel lectus vel felis."},
+    {icon:"fas fa-book-open icon",title:"Wide Renge Of Books",desc:"Aliquam dictum elit vitae mauris facilisis at dictum urna dignissim donec vel lectus vel felis."},
+    {icon:"far fa-window-restore icon",title:"Easy to Find",desc:"Aliquam dictum elit vitae mauris facilisis at dictum urna dignissim donec vel lectus vel felis."}
+  ]
   constructor() { }
 
   ngOnInit(): void {
