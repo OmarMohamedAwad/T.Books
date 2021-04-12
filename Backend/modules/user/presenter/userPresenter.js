0693f72982e5f6df776e63ddc326c60009691975
Omar.M.Awad <omar.awad825@gmail.com>
2021-04-12 03:20:12 +0200

    Edit presenter

--- Backend/modules/user/presenter/userPresenter.js
+++ Backend/modules/user/presenter/userPresenter.js
@@ -1,20 +1,19 @@
 function present(user) {
     return {
-        'id': user._id,
-        'userName': user.userName,
-        'firstName' :user.fName,
-        'lastName':user.lName,
-        'email' :user.email,
-        'avatar': user.userImage,
-        'password':user.password,
-        'fullName': user.fName +" "+  user.lName,
-        'currentlyReadedBooks':user.currentlyReadedBooks,
-        'wantToReadedBooks':user.wantToReadedBooks,
-        'readBooks':user.readBooks,
-        'userReviews':user.userReviews,
-        'userRating':user.userRatings,
-        'token ':user.refreshToken
-        
+        id: user._id,
+        userName: user.userName,
+        firstName :user.fName,
+        lastName:user.lName,
+        email :user.email,
+        avatar: user.userImage,
+        password:user.password,
+        fullName: user.fName +" "+  user.lName,
+        currentlyReadBooks:user.currentlyReadedBooks,
+        wantToReadBooks:user.wantToReadedBooks,
+        readBooks:user.readBooks,
+        userReviews:user.userReviews,
+        userRating:user.userRatings,
+        token :user.refreshToken
     }
 }
 
