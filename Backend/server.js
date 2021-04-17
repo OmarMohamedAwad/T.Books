9928cb7d96f293657c0deef05fcc389cf98c7e9f
hossamradwan <hossam.radwan.engineer@gmail.com>
2021-04-17 23:34:11 +0200

    yarab123456

--- Backend/server.js
+++ Backend/server.js
@@ -69,7 +69,7 @@ app.use("/category", categoryRouter);
 //end point for rating
 app.use("/rating", ratingRouter);
 
-app.listen(process.env.PORT, (err) => {
+app.listen(3000, (err) => {
     if (err)
         console.log("the port " + process.env.PORT + " is busy");
     else
