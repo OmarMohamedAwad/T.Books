6ffe97aa78f8b9b149623fe083be581bb4e1f47f
Omar.M.Awad <omar.awad825@gmail.com>
2021-04-20 02:30:20 +0200

    Edit book det

--- front-end/src/app/modules/user layout/book/book-det/book-det.component.ts
+++ front-end/src/app/modules/user layout/book/book-det/book-det.component.ts
@@ -1,13 +1,13 @@
-import { AfterViewInit, Component, OnInit, ViewChild, ElementRef } from '@angular/core';
-import { trigger, state, transition, animate, style } from '@angular/animations';
-import { Book } from '../../../admin layout/book/models/book';
-import { ActivatedRoute } from '@angular/router';
-import { BookServiceService } from '../../../admin layout/book/services/book-service.service';
-import { ReviewsService } from 'src/app/services/reviews.service';
-import { RatingServiceService } from '../../../../services/rating-service.service';
-import { Router } from '@angular/router';
-import { UserService } from '../../../../services/user.service';
-import { element } from 'protractor';
+import {AfterViewInit, Component, OnInit, ViewChild, ElementRef} from '@angular/core';
+import {trigger, state, transition, animate, style} from '@angular/animations';
+import {Book} from '../../../admin layout/book/models/book';
+import {ActivatedRoute} from '@angular/router';
+import {BookServiceService} from '../../../admin layout/book/services/book-service.service';
+import {ReviewsService} from 'src/app/services/reviews.service';
+import {RatingServiceService} from '../../../../services/rating-service.service';
+import {Router} from '@angular/router';
+import {UserService} from '../../../../services/user.service';
+import {element} from 'protractor';
 
 import Swal from 'sweetalert2';
 
@@ -19,36 +19,36 @@ import Swal from 'sweetalert2';
     trigger('fade', [
       //state(),
       transition('void => *', [
-        style({ opacity: 0 }),
-        animate(2000, style({ opacity: 1 }))
+        style({opacity: 0}),
+        animate(2000, style({opacity: 1}))
       ])
     ])
   ]
 })
 
-export class BookDetComponent implements OnInit,AfterViewInit {
+export class BookDetComponent implements OnInit, AfterViewInit {
 
-  @ViewChild("star1") star1!: ElementRef<HTMLInputElement>;
-  @ViewChild("star2") star2!: ElementRef<HTMLInputElement>;
-  @ViewChild("star3") star3!: ElementRef<HTMLInputElement>;
-  @ViewChild("star4") star4!: ElementRef<HTMLInputElement>;
-  @ViewChild("star5") star5!: ElementRef<HTMLInputElement>;
-  @ViewChild("reviewArea") reviewArea!: ElementRef<HTMLInputElement>;
+  @ViewChild('star1') star1!: ElementRef<HTMLInputElement>;
+  @ViewChild('star2') star2!: ElementRef<HTMLInputElement>;
+  @ViewChild('star3') star3!: ElementRef<HTMLInputElement>;
+  @ViewChild('star4') star4!: ElementRef<HTMLInputElement>;
+  @ViewChild('star5') star5!: ElementRef<HTMLInputElement>;
+  @ViewChild('reviewArea') reviewArea!: ElementRef<HTMLInputElement>;
   stars_Arr: any;
   selected: any = 'option2';
-  userId: string = "";
-  bookId: string = "";
-  user_img = "assets/user/author/author-1.jpg";
+  userId: string = '';
+  bookId: string = '';
+  user_img = 'assets/user/author/author-1.jpg';
   loading = false;
   favsNum: number = 100;
   userRate = -1;
-  userReview: string = "";
-  bookStatus: string = "";
+  userReview: string = '';
+  bookStatus: string = '';
 
   constructor(private bookService: BookServiceService, private reviewsService: ReviewsService, private userService: UserService,
-    private ratingService: RatingServiceService,
-    private myActivatedRoute: ActivatedRoute, private router: Router) {
-    this.userId = sessionStorage.getItem("userId") || ""/*"605a0532ba76f47a7793e130"*/;
+              private ratingService: RatingServiceService,
+              private myActivatedRoute: ActivatedRoute, private router: Router) {
+    this.userId = sessionStorage.getItem('userId') || ''/*"605a0532ba76f47a7793e130"*/;
   }
 
   ngAfterViewInit(): void {
@@ -57,26 +57,26 @@ export class BookDetComponent implements OnInit,AfterViewInit {
 
   book: Book =
     {
-      id: "",
-      name: "",
-      description: "",
-      image: "",
-      category: "",
-      author: "",
-      categoryName: "",
-      authorName: "",
+      id: '',
+      name: '',
+      description: '',
+      image: '',
+      category: '',
+      author: '',
+      categoryName: '',
+      authorName: '',
       bookReviews: [],
       bookRatings: [],
       currantReader: [],
       finishReadUsers: [],
       wantToReadeUsers: [],
-    }
+    };
 
   subscriber: any;
   rateSubscriber: any;
   reviewSubscriber: any;
   userSubscriber: any;
-  ratesNum: number = 0
+  ratesNum: number = 0;
   avgRate: number = 0;
   ratings: any;
   rated_before: boolean = false;
@@ -95,59 +95,70 @@ export class BookDetComponent implements OnInit,AfterViewInit {
   ngOnInit(): void {
     this.subscriber = this.bookService.show(this.myActivatedRoute.snapshot.params.id)
       .subscribe((response: any) => {
-        this.book = response.body
-        this.reviews = this.book.bookReviews;
-        this.ratings = this.book.bookRatings;
-        this.ratesNum = this.book.bookRatings.length;
-        this.readBookStatus(response.body.currantReader, response.body.wantToReadeUsers, response.body.finishReadUsers);
-        this.favsNum = 0
-        this.avgRate = 0
-        this.loading = true
-        for (let i = 0; i < this.ratings.length; i++)
-          this.avgRate += this.ratings[i].rate;
-        if (this.ratesNum)
-          this.avgRate /= this.ratesNum;
-        this.drawMyRating(this.ratings);
-      },
+          this.book = response.body;
+          this.reviews = this.book.bookReviews;
+          this.ratings = this.book.bookRatings;
+          this.ratesNum = this.book.bookRatings.length;
+          this.readBookStatus(response.body.currantReader, response.body.wantToReadeUsers, response.body.finishReadUsers);
+          this.favsNum = 0;
+          this.avgRate = 0;
+          this.loading = true;
+          for (let i = 0; i < this.ratings.length; i++) {
+            this.avgRate += this.ratings[i].rate;
+          }
+          if (this.ratesNum) {
+            this.avgRate /= this.ratesNum;
+          }
+          this.drawMyRating(this.ratings);
+        },
         (err) => {
           Swal.fire({
             icon: 'error',
             title: 'Oops...',
-            text: "Error getting book details!",
+            text: 'Error getting book details!',
             footer: ''
-          })
+          });
         }
-      )
+      );
   }
 
   setRate(bookRate: any) {
-    console.log("here")
+    console.log('here');
     if (this.userId) {
       if (!this.rated_before) {
-        this.rateSubscriber = this.ratingService.store({ rate: bookRate, rater: this.userId, book: this.book.id }).subscribe((response: any) => { },
+        this.rateSubscriber = this.ratingService.store({
+          rate: bookRate,
+          rater: this.userId,
+          book: this.book.id
+        }).subscribe((response: any) => {
+          },
           (err) => {
             Swal.fire({
               icon: 'error',
               title: 'Oops...',
-              text: "Error, your rate hasn't been saved !",
+              text: 'Error, your rate hasn\'t been saved !',
               footer: ''
-            })
+            });
           }
-        )
-      }
-      else {
-        this.rateSubscriber = this.ratingService.update({ rate: bookRate, rater: this.userId, book: this.book.id }).subscribe((response: any) => { },
-          (err)=>{
+        );
+      } else {
+        this.rateSubscriber = this.ratingService.update({
+          rate: bookRate,
+          rater: this.userId,
+          book: this.book.id
+        }).subscribe((response: any) => {
+          },
+          (err) => {
             Swal.fire({
               icon: 'error',
               title: 'Oops...',
-              text: "Error, your rate hasn't been updated !",
+              text: 'Error, your rate hasn\'t been updated !',
               footer: ''
-            })
+            });
           }
-        )
+        );
       }
-    }
+    }else {
       Swal.fire({
         icon: 'error',
         title: 'Oops...',
@@ -179,20 +190,20 @@ export class BookDetComponent implements OnInit,AfterViewInit {
     if (this.userId) {
       this.text = this.reviewArea.nativeElement.value;
       if (this.text.length >= 1 && this.text.length <= 300) {
-        this.reviewerId = /*sessionStorage.getItem("userId");*/ this.userId
-        this.reviewSubscriber = this.reviewsService.store({ reviwer: this.reviewerId, book: this.book.id, body: this.text })
+        this.reviewerId = /*sessionStorage.getItem("userId");*/ this.userId;
+        this.reviewSubscriber = this.reviewsService.store({reviwer: this.reviewerId, book: this.book.id, body: this.text})
           .subscribe((response: any) => {
-            console.log(response)
+            console.log(response);
             // this.router.navigate([`/book/${this.myActivatedRoute.snapshot.params.id}`]);
-            this.reloadComponent()
+            this.reloadComponent();
           }, (err) => {
             Swal.fire({
               icon: 'error',
               title: 'Oops...',
-              text: "Error, your review hasn't been saved !",
+              text: 'Error, your review hasn\'t been saved !',
               footer: ''
-            })
-          })
+            });
+          });
       }
     } else {
       Swal.fire({
@@ -244,9 +255,9 @@ export class BookDetComponent implements OnInit,AfterViewInit {
     if (currant) {
       this.bookStatus = 'Is currant read';
     } else if (want) {
-      this.bookStatus = "Want to read"
+      this.bookStatus = 'Want to read';
     } else if (finish) {
-      this.bookStatus = "Finished reading"
+      this.bookStatus = 'Finished reading';
     } else {
       this.bookStatus = "Add to my list"
     }
